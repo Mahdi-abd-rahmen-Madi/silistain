95576771f568685d5470251e339dc073f39ce309
mahdi <mahdiabderrahmen@tutnanota.com>
2025-10-04 11:27:55 +0100

    prod10

--- watch-shop/vite.config.js
+++ watch-shop/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
     publicDir: 'public',
     plugins: [react()],
     resolve: {
